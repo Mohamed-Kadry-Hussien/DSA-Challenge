b5a96b63bff34235980197e4852fda8fddf56738
Mohamed Kadry <mokadry2004@gmail.com>
2025-06-26 17:31:24 +0300

    اللهم إني أسألك خير المسألة، وخير الدعاء، وخير النجاح، وخير العمل، وخير الثواب، وخير الحياة

---
+++ Array/Valid_Anagram.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/valid-anagram/?envType=problem-list-v2&envId=plakya4j&
+//Solution by Array
+class Solution {
+    public boolean isAnagram(String s, String t) 
+    {
+        int len1=s.length(); 
+        int len2=t.length();
+        if(len1!= len2 )
+            return false;
+        
+        int[] chars = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) 
+        {
+            chars[s.charAt(i)-'a']++;
+            chars[t.charAt(i)-'a']--;
+        }
+        
+        for(int i=0 ; i<26 ; i++) {
+            if(chars[i]!= 0)
+            return false;
+        }
+
+        return true;
+    }
+}
--- Hashing/Valid_Anagram.java
+++ Hashing/Valid_Anagram.java
@@ -1,25 +1,24 @@
 //https://leetcode.com/problems/valid-anagram/?envType=problem-list-v2&envId=plakya4j&
+//Solution by Hashing
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        int len1=s.length(); 
-        int len2=t.length();
-        if(len1!= len2 )
-            return false;
-        
-        int[] chars = new int[26];
-        
-        for(int i = 0; i < s.length(); i++) 
-        {
-            chars[s.charAt(i)-'a']++;
-            chars[t.charAt(i)-'a']--;
-        }
-        
-        for(int i=0 ; i<26 ; i++) {
-            if(chars[i]!= 0)
-            return false;
-        }
-
-        return true;
+       Map <Character,Integer> count = new HashMap<>();
+       char [] str1 = s.toCharArray();
+       char [] str2 = t.toCharArray();
+       for(char x : str1)
+       {
+         count.put(x ,count.getOrDefault(x,0)+1); //if char (is element in HashMap) =>  return frequency of it and (freq +1)
+       }  
+       for(char x : str2)
+       {
+         count.put(x ,count.getOrDefault(x,0)-1); //if char (is element in HashMap) =>  return frequency of it and (freq -1) 
+       } 
+       for(int val : count.values())
+       {
+         if(val!=0)
+         return false;
+       }  
+        return true ;    
     }
 }
